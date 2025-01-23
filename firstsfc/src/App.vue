7fbd266ac92d25a596d69db96b00a20e9a94a008
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-01-23 06:49:40 +0000

    adding the component

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <h1>Hello WEBPROG SF 231 World!</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <script></script>
-<style></style>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+  #app > div:hover {
+  cursor: pointer;
+  }
+</style>
