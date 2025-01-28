1e7cb650e21b319ddfad7dbddecbb8a3cbf37a52
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-01-28 20:17:51 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -2,11 +2,11 @@
 
 <template>
    <h1>Food</h1>
-   <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+  <personal-profile/> <br>
+  <food-item/> 
+  <food-item2/>
+  <food-item/> 
+  <food-item2/> 
 </template>
 
-<style></style>  
+<style></style>
