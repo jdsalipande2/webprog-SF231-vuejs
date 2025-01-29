b9c222d9c03dcbaf3583742bd046f5fc53a7c99c
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-01-29 19:31:32 +0800

    Update PersonalProfile.vue

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -325,6 +325,25 @@
             </section>
          </div>
       </div>
+      <!-- Footer Section -->
+      <footer class="footer">
+         <div class="footer-container">
+            <img src="/assets/jmds%20logo%20white%20version.png" alt="My Logo" class="footer-logo"> 
+            <p class="footer-text">© 2025 Janice Maxene D. Salipande | All Rights Reserved.</p>
+            <div class="footer-socials">
+               <a href="https://www.linkedin.com/in/janice-maxene-salipande-8aa856216" target="_blank" class="social-icon">
+                  <div class="social-circle">
+                     <img src="/assets/linkedin%20icon.png" alt="LinkedIn Icon" class="social-img">
+                  </div>
+               </a>
+               <a href="mailto:jdsalipande2@student.apc.edu.ph" target="_blank" class="social-icon">
+                  <div class="social-circle">
+                     <img src="/assets/mail%20icon.png" alt="Mail Icon" class="social-img">
+                  </div>
+               </a>
+            </div>
+         </div>
+      </footer>
    </div>
 </template>
 
