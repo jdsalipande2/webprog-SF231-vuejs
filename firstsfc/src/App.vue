deb96d8fb7c90deece86b0a560a4a34765aa914f
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-01-30 07:19:35 +0000

    supabase again

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+  </script>
  
-  const countries = ref([])
- 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
- 
-  onMounted(() => {
-    getCountries()
-  })
-</script>
- 
-<template>
 <template>
     <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
