8e553812d00888859244ef8bfe82bb3d0daa5b6e
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-03 14:50:45 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <div>
     <PersonalProfile />
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
   </div>
 </template>
 
 <script>
 import PersonalProfile from './components/PersonalProfile.vue';
-import { supabase } from './lib/supabaseClient';
 
 export default {
   components: {
     PersonalProfile
-  },
-  data() {
-    return {
-      countries: []
-    };
-  },
-  methods: {
-    async getCountries() {
-      const { data } = await supabase.from('countries').select();
-      this.countries = data || [];
-    }
-  },
-  mounted() {
-    this.getCountries();
   }
 };
 </script>
