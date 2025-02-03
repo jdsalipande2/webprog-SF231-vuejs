428c2100d382326a198486898c0eb09918be0497
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-03 14:47:57 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,31 +1,37 @@
-<script setup>
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
-
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-  <h1>Food</h1>
-  <PersonalProfile /> <br>
-  <FoodItem /> <br>
-  <FoodItem2 /> <br>
-  <FoodItem /> <br>
-  <FoodItem2 /> <br>
+  <div>
+    <PersonalProfile />
+    <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
+  </div>
 </template>
 
-<style></style>
+<script>
+import PersonalProfile from './components/PersonalProfile.vue';
+import { supabase } from './lib/supabaseClient';
+
+export default {
+  components: {
+    PersonalProfile
+  },
+  data() {
+    return {
+      countries: []
+    };
+  },
+  methods: {
+    async getCountries() {
+      const { data } = await supabase.from('countries').select();
+      this.countries = data || [];
+    }
+  },
+  mounted() {
+    this.getCountries();
+  }
+};
+</script>
+
+<style>
+/* Your global styles here */
+</style>
