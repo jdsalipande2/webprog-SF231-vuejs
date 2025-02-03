6b08b41197e2668e00c454dec951f18d342997bc
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-03 14:37:23 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import PersonalProfile from './components/PersonalProfile.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import { ref, onMounted } from 'vue'
