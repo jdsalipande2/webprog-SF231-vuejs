802b869e4b8a17e1896fd2ceb8ae73cdaa5965b7
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-03 14:33:39 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,35 +1,32 @@
 <script setup>
-  import PersonalProfile from './components/PersonalProfile.vue';
+import PersonalProfile from './components/PersonalProfile.vue'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
-  export default {
-  components: {
-    PersonalProfile
-  }
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+const countries = ref([])
 
-  const countries = ref([])
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
 
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
+onMounted(() => {
+  getCountries()
+})
+</script>
 
-  onMounted(() => {
-    getCountries()
-  })
-  </script>
- 
 <template>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-   <h1>Food</h1>
-   <personal-profile/> <br>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+  <h1>Food</h1>
+  <PersonalProfile /> <br>
+  <FoodItem /> <br>
+  <FoodItem2 /> <br>
+  <FoodItem /> <br>
+  <FoodItem2 /> <br>
 </template>
 
-<style></style>  
+<style></style>
