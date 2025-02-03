57fe7f8373bfa48d99d63188a71109c56726bb4f
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-03 14:26:32 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import './css/styles.css';
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
