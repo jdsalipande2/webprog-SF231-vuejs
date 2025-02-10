b5050333db4e240cab6dc142254cc72ac6fa6a64
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-10 07:30:49 +0000

    updated other files

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,10 +4,12 @@ import FoodItem from './components/FoodItem.vue'
 import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('country', Country)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
